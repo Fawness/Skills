2c2f86f33e09cfe75cdc190160c084d68430d2cc
Fawness <moonflarefox@gmail.com>
2020-03-09 09:58:34 -0700

    Worked on Website

--- Recipe/src/app/app.module.ts
+++ Recipe/src/app/app.module.ts
@@ -1,21 +1,33 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
-//components 
-import { AppComponent } from './app.component';
-
-//services
+// services
 import { RecipeService } from './services/recipe.service';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    EditRecipeComponent,
+    NewRecipeComponent,
+    ShowRecipeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [
     RecipeService
--- Recipe/src/app/classes/recipe.ts
+++ Recipe/src/app/classes/recipe.ts
@@ -11,7 +11,13 @@ export class Recipe implements IRecipe {
     instructions: string[];
 
     constructor({id, title, description, serves, imageUrl, ingredients, instructions}) {
-
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.serves = serves;
+        this.imageUrl = imageUrl;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
 
     }
 
